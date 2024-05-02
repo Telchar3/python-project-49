eea817f42fa156acd06623c4d377ab8c03c9d693
Bulat Dugarov <erdembulat03@gmail.com>
2024-05-02 05:31:57 -0400

    add in brain_games.py call function from cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 
 def main():
     print ('Welcome to the Brain Games!')
+    from brain_games.cli import welcome_user
+    welcome_user()
 
 if __name__ == '__main__':
     main()
