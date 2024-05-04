694855bcdf0c068960dfcffab3ee6bf93d80f0b2
Bulat Dugarov <erdembulat03@gmail.com>
2024-05-04 15:46:00 -0400

    improve brain_even.py from flake8

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,15 @@
 import prompt
 tried = 0
+
+
 def even_number(name):
     from random import randint
     global tried
-    random_number = randint(1,100)
-    print (f'Question: {random_number}')
+    random_number = randint(1, 100)
+    print(f'Question: {random_number}')
     answer = prompt.string('Your answer: ')
-    if random_number % 2 == 0 and answer.lower() == 'yes' or random_number % 2 == 1 and answer.lower() == 'no':
+    if random_number % 2 == 0 and answer.lower() == 'yes' \
+            or random_number % 2 == 1 and answer.lower() == 'no':
         print('Correct!')
         tried = tried + 1
     elif random_number % 2 == 0:
@@ -17,6 +20,7 @@ def even_number(name):
         print(f"{answer} is wrong answer ;(. Correct answer was 'no'")
         print(f"Let's try again, {name}")
         tried = 100
+
 
 def main():
     print('Welcome to the Brain Games')
